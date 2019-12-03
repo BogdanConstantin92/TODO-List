158d0c53a3202b59ed0428c5680d88cd0ecb1199
BogdanConstantin92 <bogdan_porojan@yahoo.com>
2019-12-03 14:39:46 +0200

    Updates

---
+++ precache-manifest.be9c0fdc1f98271b7b94d1c1f36503fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88fca8127d43a51c9226dcdd4577619e",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "5fbe0857c854dbe41c23",
+    "url": "/todo-list/static/css/main.f244714d.chunk.css"
+  },
+  {
+    "revision": "335f20a5e6c5dffb1916",
+    "url": "/todo-list/static/js/2.af257eb6.chunk.js"
+  },
+  {
+    "revision": "5fbe0857c854dbe41c23",
+    "url": "/todo-list/static/js/main.241fd7b1.chunk.js"
+  },
+  {
+    "revision": "3254e21177d6b231e08e",
+    "url": "/todo-list/static/js/runtime-main.4cee20ac.js"
+  }
+]);
